b0fe357f95867634ba47cbf23bd4a9766cea5655
Dhanaush12212 <dhanush12212@gmail.com>
2024-11-11 23:28:53 +0530

    Change the background image and did some update

--- echovote-frontend/src/Pages/LandingPage.jsx
+++ echovote-frontend/src/Pages/LandingPage.jsx
@@ -1,30 +1,44 @@
-import React from 'react'
-import { assets } from '../assets/assets'
-import {Link} from 'react-router-dom'; 
+import React from "react";
+import { assets } from "../assets/assets";
+import { Link } from "react-router-dom";
 
 function LandingPage() {
   return (
     <>
-      <div className="relative flex flex-col items-center justify-center h-screen bg-gray-100"> 
-      <div className="absolute w-full h-screen">
-        <img className="w-full h-full object-cover md:h-full" src={assets.BackgroundImg}  alt="Landing page background" /> 
-      </div>  
-      <div className="Heading relative flex flex-col items-center text-center p-6 bg-opacity-50 bg-white rounded-lg max-w-lg md:max-w-2xl lg:max-w-4xl mx-0 h-[50%] justify-center gap-3"> 
-
-        <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold text-[#1E8DD6ff] mb-4 ">Choose the music <span className='text-black'>that sets the mood! </span></h1> 
-        <p className='text-lg sm:text-md'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nemo, cumque accusantium. Soluta quia nesciunt officia, nulla rerum repellendus </p>
-
-        <div className='flex gap-3'> 
-        <Link to="/playList/:venueName"> <li className="list-none mt-8 px-4 sm:px-6 py-3 bg-[#027ED1ff] text-white font-semibold rounded-full hover:bg-blue-700 transition duration-200 text-sm sm:text-base">Get Started</li></Link>
-        <Link to="/loginPage"><li className="list-none mt-8 px-4 sm:px-6 py-3 bg-[#027ED1ff] text-white font-semibold rounded-full hover:bg-blue-700 transition duration-200 text-sm sm:text-base">
-        Log In / Sign Up</li></Link>
-          
+      <div className="relative flex flex-col items-center justify-center h-screen bg-gray-100">
+        <div className="absolute w-full h-screen">
+          <img
+            className="w-full h-full object-cover md:h-full"
+            src={assets.PlayListBackground}
+            alt="Landing page background"
+          />
         </div>
+        <div className="Heading relative flex flex-col items-center text-center p-6 bg-opacity-50 bg-white rounded-lg max-w-lg md:max-w-2xl lg:max-w-4xl mx-0 h-[50%] justify-center gap-3">
+          <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold text-[#1E8DD6ff] mb-4 ">
+            Choose the music{" "}
+            <span className="text-black">that sets the mood! </span>
+          </h1>
+          <p className="text-lg sm:text-md">
+          Transform the atmosphere in bars, gyms, and restaurants with Echovote. Our innovative platform puts the power of music selection directly into the hands of your customers. 
+          </p>
 
+          <div className="flex gap-3">
+            <Link to="/playList/:venueName">
+              {" "}
+              <li className="list-none mt-8 px-4 sm:px-6 py-3 bg-[#027ED1ff] text-white font-semibold rounded-full hover:bg-blue-700 transition duration-200 text-sm sm:text-base active:scale-90">
+                Get Started
+              </li>
+            </Link>
+            <Link to="/loginPage">
+              <li className="list-none mt-8 px-4 sm:px-6 py-3 bg-[#027ED1ff] text-white font-semibold rounded-full hover:bg-blue-700 transition duration-200 text-sm sm:text-base active:scale-90">
+                Log In / Sign Up
+              </li>
+            </Link>
+          </div>
+        </div>
       </div>
-    </div>  
     </>
-  )
+  );
 }
 
-export default LandingPage
+export default LandingPage;
